7221b2817246f217e80b7dc84c7121ae203b4bf7
Salomone Baquis <salomone.l.baquis@gmail.com>
2018-01-11 20:13:03 -0500

    this worked but was denied

--- api/managed-records.js
+++ api/managed-records.js
@@ -5,27 +5,30 @@ import URI from "urijs";
 window.path = "http://localhost:3000/records";
 
 let options = {
-	colors: ["red"]
+	page: 10,
+	colors: ["red", "blue"]
 }
 
-retrieve(options)
+retrieve(options).then(result => console.log(result))
 
 function retrieve(options) {
 	let endpointObject = createUri(options)
 	let endpoint = endpointObject.href()
-	let unboundedEndpoint = unboundEndpoint(endpointObject).href()
-	console.log(endpoint, "\n", unboundedEndpoint)
-	Promise.all([rp(endpoint), rp(unboundedEndpoint)]) // replace with fetch
-		.then(results => {
-			// check status code here
-			let pageResults = JSON.parse(results[0]) // remove json.parse if necessary
-			let allResults = JSON.parse(results[1])
-			let transformedResults = {}
-			transform(pageResults, allResults, transformedResults)
-			console.log(transformedResults)
-			return transformedResults
-		})
-		.catch(error => { console.log("\x1b[31m%s%s\x1b[0m", "Error: ", error.message, error) })
+	let unboundedEndpoint = increaseEndpoint(endpointObject).href()
+	return callApi(endpoint, unboundedEndpoint)
+}
+function callApi(endpoint, unboundedEndpoint) {
+	return Promise.all([rp(endpoint), rp(unboundedEndpoint)]) // replace with fetch
+		.then(results => handleResults(results))
+		.catch(error => console.log("\x1b[31m%s%s\x1b[0m", "Received the following error: \n\n", error.message))
+}
+function handleResults(results) {
+	// check status code here
+	let pageResults = JSON.parse(results[0]) // remove json.parse if necessary
+	let allResults = JSON.parse(results[1])
+	let transformedResults = {}
+	transform(pageResults, allResults, transformedResults)
+	return transformedResults	
 }
 function createUri(options) {
 	let uri = URI("http://localhost:3000/records")
@@ -34,17 +37,15 @@ function createUri(options) {
 	if (options.colors && options.colors.length) { uri.setSearch("color[]", options.colors) }
 	return uri
 }
-function transform(pageResults, allResults, transformObject) {
+function transform(pageResults, allResults, transformedResults) {
 	let pagination = getPagination(pageResults, allResults)
-	transformObject["ids"] = pageResults.map(item => item.id)
-	transformObject["open"] = getOpenResults(pageResults)
-	transformObject["closedPrimaryCount"] = getClosedPrimaryCount(pageResults)	
-	transformObject["previousPage"] = pagination.previousPage
-	transformObject["nextPage"] = pagination.nextPage
-	return transformObject
+	transformedResults["ids"] = pageResults.map(item => item.id)
+	transformedResults["open"] = getOpenResults(pageResults)
+	transformedResults["closedPrimaryCount"] = getClosedPrimaryCount(pageResults)	
+	transformedResults["previousPage"] = pagination.previousPage
+	transformedResults["nextPage"] = pagination.nextPage
+	return transformedResults
 }
-
-/* ------ Helper Functions ------ */
 function getOpenResults(results) {
 	let openResults = results.filter(item => item.disposition === "open")
 	openResults.map(item => item["isPrimary"] = testPrimary(item) ? true : false)
@@ -54,15 +55,11 @@ function getClosedPrimaryCount(results) {
 	let closedPrimaryColorResults = results.filter(item => item.disposition === "closed" && testPrimary(item) === true)
 	return closedPrimaryColorResults.length
 }
-function testPrimary(item) {
-	return (item.color === "red" || item.color === "blue" || item.color === "yellow") ? true : false
-}
 function getPagination(pageResults, allResults) {
 	var pagination = {}
 	if (pageResults.length) {	
 		let lowestResultIndex = allResults.findIndex( item => lowIndexHelper(item, pageResults))
 		let highResultIndex = allResults.findIndex( item => highIndexHelper(item, pageResults))
-		
 		// Check low results
 		if (lowestResultIndex < 10 || lowestResultIndex === -1) {
 			pagination["previousPage"] = null
@@ -83,15 +80,19 @@ function getPagination(pageResults, allResults) {
 	}
 	return pagination
 }
-
-// These find the indices of the lowest and highest paginated results within the full results
+/* ------ SMALL HELPERS ------ */
+function testPrimary(item) {
+	return (item.color === "red" || item.color === "blue" || item.color === "yellow") ? true : false
+}
+// Get the indices of the lowest and highest paginated results within the full results
 function lowIndexHelper(item, pageResults) {
 	return item.id === pageResults[0].id
 }
 function highIndexHelper(item, pageResults) {
 	return item.id === pageResults[pageResults.length-1].id
 }
-function unboundEndpoint(endpointObject) {
+// Increase the endpoint limit so we can fetch all results
+function increaseEndpoint(endpointObject) {
 	endpointObject.setSearch("limit", 1000)
 	if (endpointObject.hasSearch("offset")) { endpointObject.removeSearch("offset") }
 	return endpointObject
